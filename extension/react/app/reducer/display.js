70a48794b86879111b3aab0748f52617d4c422a4
sgordon834 <sgordon834@gmail.com>
2018-02-03 15:54:25 -0800

    profile components hide/show work

--- extension/react/app/reducer/display.js
+++ extension/react/app/reducer/display.js
@@ -2,9 +2,10 @@ import signup from "../../components/profile/_profile";
 
 let defaultState = {
 home: false,
-signup: true,
+signup: false,
 verify: false,
-unlock: true
+unlock: false,
+profile: true
 }
 
 export default (state=defaultState, action) => {
