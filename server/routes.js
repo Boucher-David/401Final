66c3bb57e46841fdb9d8b9bccb5eb7547045a11d
David Boucher <david.boucher@ookla.com>
2018-02-03 09:57:31 -0800

    Done

--- server/routes.js
+++ server/routes.js
@@ -5,12 +5,11 @@ const app = module.exports = express.Router();
 const authHeader = require('./lib/authHeader.js');
 const User = require('./schema/User');
 
-const findUser = require('./lib/asyncHelpers');
-
+const userHelper = require('./lib/userHelper');
 
 app.use(authHeader);
 
-// Useful for testing signup. Deletes account before route is run.
+//Useful for testing signup. Deletes account before route is run.
 // app.use((req, res, next) => {
 //     User.findOneAndRemove({username: 'username'}).then(response => {
 //         return next();
@@ -22,7 +21,7 @@ app.use(authHeader);
 const email = require('./lib/email');
 
 
-app.post('/profile/signup', (req, res, next) => {
+app.post('/profile/signup', async (req, res, next) => {
 
     res.body = res.body || {};
     res.body.vault = res.body.vault || {};
@@ -35,76 +34,75 @@ app.post('/profile/signup', (req, res, next) => {
         password: credentials.password
     });
 
+    this._checkUsername = await userHelper.findUser({username: credentials['username']});
+    this._checkEmail = await userHelper.findUser({email: credentials['email']});
 
-    User.findOne({username: credentials['username']}).then((response) => {
-        if (response) {
-            res.body.vault = {
-                signup: false,
-                message: 'Username taken.'
-            }
 
-            res.send("Already created");
-        } else {
-            User.findOne({email: credentials['email']}).then(response => {
-                if (response) {
-                    res.body.vault = {
-                        signup: false,
-                        message: 'Email taken.'
-                    }
-                    res.send("Already created");
-                } else {
-                    newUser.hashPassword(newUser['password']).then(hash => {
-                        newUser.password = hash.password;
-                        newUser.user_id = hash.user_id;
-                        newUser.verify = false;
-                        newUser.verifyCode = hash.verifyCode;
-                        newUser.save().then(response => {
-                            res.body.vault.signup = true;
 
-                            // send verify code to verify account.
-
-                            res.send("Done.")
-                        });
-                    })
-                }
-            })
+    if (this._checkUsername || this._checkEmail) {
+        res.body.vault = {
+            signup: false,
+            message: 'Account Taken.'
         }
-    })
-  
+        res.send("Account already taken.");
+        return next();
+    } else {
+        newUser.hashPassword(newUser['password']).then(hash => {
+            newUser.password = hash.password;
+            newUser.user_id = hash.user_id;
+            newUser.verify = false;
+            newUser.verifyCode = hash.verifyCode;
+            newUser.save().then(response => {
+                res.body.vault.signup = true;
+
+                // send verify code to verify account.
+
+                res.send("Done.")
+                return next();
+            });
+        })
+    }
 });
 
-app.post('/profile/signin', (req, res, next) => {
+app.post('/profile/signin', async (req, res, next) => {
     let credentials = req.body.vault.auth.basic;
 
     res.body = res.body || {};
     res.body.vault = res.body.vault || {};
     res.body.vault.signin = false;
 
-    User.findOne({username: credentials['username']}).then(response => {
-        if (!response) return res.send("Done");
-        if(!response.verified) {
-            res.body.vault.message = "Account unverified";
-            return res.send("Done");
+
+    this._checkUsername = await userHelper.findUser({username: credentials['username']});
+    this._checkEmail = await userHelper.findUser({email: credentials['email']});
+
+    if (this._checkUsername && this._checkEmail) {
+        if (!this._checkUsername.verify) {
+            res.body.vault = {
+                signin: false,
+                message: "User is unverified"
+            };
+            res.send(res.body.vault);
+            return next();
         }
 
-            User.findOne({email: credentials['email']}).then(user=> {
-                if (!user) return res.send("Done");
-                    response.compare(credentials['password'], response.password).then(response => {
-                        if (response) {
-                            res.body.vault.signin = true;
-                            res.body.vault.user = user.user_id;
-                        }
-                        res.send("Done.");
-                    });
+        this._checkPassword = await userHelper.compare(credentials['password'], this._checkUsername.password);
+        if (this._checkPassword) {
+            res.body.vault = {
+                signin: true,
+                user_id: this._checkUsername.user_id
+            }
+        }
 
-            });
-
-        
-    });
-
+        res.send(res.body.vault);
+        return next();
+    } else {
+        res.send(res.body.vault);
+        return next();
+    }
 });
 
-app.get('/', async (req, res, next) => {
-    let foundUser = await findUser({username: 'username', password: null});
-    res.send(foundUser);
+app.get('/*', async (req, res, next) => {
+    let _findUser = await userHelpers.findUser(credentials);
+
+    res.send(_findUser);
 });
--- server/schema/asyncHelpers.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-
-module.exports = findUser = async (credentials) => {
-    return  _user = await User.findOne({...credentials});
-}
