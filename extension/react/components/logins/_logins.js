c78a7fe540eceb7b304a9e5d40e26139805dd18f
sgordon834 <sgordon834@gmail.com>
2018-02-08 20:29:09 -0800

    still working on logins

--- extension/react/components/logins/_logins.js
+++ extension/react/components/logins/_logins.js
@@ -5,80 +5,104 @@ import Collapsible from 'react-collapsible';
 class Logins extends React.Component {
 
             state = {
-            login: ['facebook', 'amazon', 'google'],
+            logins: ['facebook', 'amazon', 'google'],
             message: 'hello',
             credentials: {
-                username: 'Vault Team',
-                email: 'email',
-                password: 'password'
+                amazon: {
+                    username: 'username',
+                    email: 'email',
+                    password: 'password'
+                },
+                facebook: {
+                    username: 'username',
+                    email: 'email',
+                    password: 'password'
+                },
+                google: {
+                    username: 'username',
+                    email: 'email',
+                    password: 'password'
+                }
+
+                
             }
         
-
+                
        
     }
 
+    handleChange = (e) => {
+        console.log(e.target.value, 'target')
+    
+        let {name, value} = e.target;
+    
+        this.setState({[name]:value});
+      }
+
+    //   category = () => {
+    //     this.state.logins.map((login, i) => {
+    //         let category = {};
+    //         category[login] = {};
+    //         this.setState({credentials: category});
+    //     })
+    //     return category();
+    //     console.log(this.state);
+    //   }
+
 
   render() {
-    console.log(Object.entries(this.state.credentials))
     return(
 
-    <div>
-        
-      <Collapsible trigger='FB'>
-    {/* {Object.keys(this.state.credentials).map((value, i) => <h1 key={i}> {value} Sidney </h1>)} */}
-
-    {Object.entries(this.state.credentials).map((value, i) => 
-            <div>
-                <p>{value[0]}:</p>
-                <input 
-                key={i}
-                type={value[0] === 'password' ? 'password' : null}
-                value={value[1]}
-                name={value[0]}
-                />
-            </div>
-            )}
-      </Collapsible>
-       
-
-         <div>
-        <Collapsible trigger='amazon'>
-        {Object.entries(this.state.credentials).map((value, i) => 
-            <div>
-                <p>{value[0]}:</p>
-                <input 
-                key={i}
-                type={value[0] === 'password' ? 'password' : 'text'}
-                value={value[1]}
-                name={value[0]}
-                />
-            </div>
-            )}
-        </Collapsible>
-        </div> 
-
         <div>
-        <Collapsible trigger='google'>
-        {Object.entries(this.state.credentials).map((value, i) => 
-            <div>
-                <p>{value[0]}:</p>
-                <input 
-                key={i}
-                type={value[0] === 'password' ? 'password' : 'text'}
-                value={value[1]}
-                name={value[0]}
-                />
-            </div>
-            )}
-         </Collapsible>
-         </div> 
-         
+
+            { 
+                this.state.logins.map( (login,i) =>
+                    <Collapsible trigger={login}>
+                    
+                        {
+                            Object.entries(this.state.credentials[login]).map( (cred,i) => {
+                            
+                                <div key={i}>
+                                    <p>{cred}:</p>
+                                    <input 
+                                    type={cred === 'password' ? 'password' : null}
+                                    value={this.state.credentials[login][cred]}
+                                    name={cred}
+                                    />
+                                </div>
+                            })
+                        }       
+
+                    </Collapsible>
+                )
+            }
+
          </div>
 
     );
   }
 
 }
+
+
+{/* <Collapsible trigger={login}>
+                    
+{
+    Object.keys(this.state.credentials[login]).map( (cred,i) => {
+    
+        <div>
+            <p>{this.state.credentials[login]['username']}:</p>
+            <input 
+            key={i}
+            type={cred === 'password' ? 'password' : null}
+            value={this.state.credentials[login][cred]}
+            name={cred}
+            />
+        </div>
+    })
+}       
+
+</Collapsible> */}
 
 export default Logins;
 
